951aae3209d96d4ec853f9e00a712d6cd366fe7a
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-09-19 23:45:34 -0300

    Arrays

--- web-moderno-master/exercicios-web/fundamentos/11.arrays.js
+++ web-moderno-master/exercicios-web/fundamentos/11.arrays.js
@@ -4,13 +4,19 @@ console.log(valores[4])
 
 valores[4] = 10
 console.log(valores)
+//How many Fields the array has, in this case has 5
 console.log(valores.length)
 
+//Created a new array with different data types
 valores.push({id: 3}, false, null, 'teste')
 console.log(valores)
 
+// Delete the last field of the array
 console.log(valores.pop())
+
+// Delete the first field of the array
 delete valores[0]
 console.log(valores)
 
+// Type object
 console.log(typeof valores)
