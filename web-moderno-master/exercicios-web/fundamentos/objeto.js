15d2ea0860e30b97727c8de631c8da1008ebfb54
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-09-20 22:05:07 -0300

    Object

--- web-moderno-master/exercicios-web/fundamentos/objeto.js
+++ web-moderno-master/exercicios-web/fundamentos/objeto.js
@@ -1,4 +1,5 @@
 const prod1 = {}
+// Cria os atributos dinamicamente, um object can has other object inside
 prod1.nome = 'Celular Ultra Mega'
 prod1.preco = 4998.90
 prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
@@ -7,7 +8,16 @@ console.log(prod1)
 
 const prod2 = {
     nome: 'Camisa Polo',
-    preco: 79.90
+    preco: 79.90,
+    //An object inside another one
+    //Object name cannot be repeated
+    obj: {
+        blabla: 1,
+        obj: {
+            blabla: 2,
+            MikhaPassedHere: true
+        }
+    }
 }
 
 console.log(prod2)
