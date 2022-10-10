07bcfb4be1a9c77bb77cff8b2fe19c8831bf23cb
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-10-09 22:14:57 -0300

    Using Var in a Loop 2

--- web-moderno-master/exercicios-web/fundamentos/22.usandoVarEmLoop2.js
+++ web-moderno-master/exercicios-web/fundamentos/22.usandoVarEmLoop2.js
@@ -1,10 +1,12 @@
 const funcs = []
 
 for (var i = 0; i < 10; i++) {
+    //Adding a new element to the Array
     funcs.push(function() {
         console.log(i)
     })
 }
 
+//Both are 10, even though they are in different positions
 funcs[2]()
 funcs[8]()
