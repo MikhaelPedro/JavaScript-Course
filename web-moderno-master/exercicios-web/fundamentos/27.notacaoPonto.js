4b35f93190ae4a9f5fe9da329b99162cf7531332
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2023-02-13 21:14:20 -0300

    Using dot to access properties

--- web-moderno-master/exercicios-web/fundamentos/27.notacaoPonto.js
+++ web-moderno-master/exercicios-web/fundamentos/27.notacaoPonto.js
@@ -1,11 +1,13 @@
 console.log(Math.ceil(6.1))
 
 const obj1 = {}
+//It is dynamically created when you try to access an object's variable
 obj1.nome = 'Bola'
-// obj1['nome'] = 'Bola2'
+// obj1['nome'] = 'Bola2' // You can still access an object's variable this way
 console.log(obj1.nome)
 
 function Obj(nome) {
+    //Using (this) objects become public
     this.nome = nome
     this.exec = function() {
         console.log('Exec...')
