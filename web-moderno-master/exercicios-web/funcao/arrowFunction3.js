b6f4ae702faa19cfccb6edd1db3edb12d9ac5b60
MikhaelPedro <mikhaelpedro2000@gmail.com>
2023-08-24 21:42:05 -0300

    Arrow function part 3

--- web-moderno-master/exercicios-web/funcao/arrowFunction3.js
+++ web-moderno-master/exercicios-web/funcao/arrowFunction3.js
@@ -12,7 +12,9 @@ comparaComThis(obj)
 let comparaComThisArrow = param => console.log(this === param)
 comparaComThisArrow(global)
 comparaComThisArrow(module.exports)
+comparaComThisArrow(this) // this in an arrow function is 'module.exports'
 
 comparaComThisArrow = comparaComThisArrow.bind(obj)
 comparaComThisArrow(obj)
 comparaComThisArrow(module.exports)
+comparaComThisArrow(this)
